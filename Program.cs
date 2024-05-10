ed5c0374f803c86a515966b6acd9e133cc53cf52
Jorge-Marcelo <jorge3258@hotmail.com>
2024-05-10 13:43:07 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Retangulo_Exercicio_POO
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+   Retangulo ret = new Retangulo();
+
+   Console.WriteLine("Entre com a altura e a largura do retangulo");
+   ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+   ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+   Console.WriteLine($"Area: {ret.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+   Console.WriteLine($"Perimetro: {ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+   Console.WriteLine($"Diagonal: {ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+  }
+ }
+}
